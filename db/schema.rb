82913ed5a41c23a83373ca317d8d0258768dc007
tooya-a <ayaka.toya@divx.co.jp>
2023-10-20 11:12:53 +0900

    バリデーションの追加

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_20_012830) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_20_015745) do
   create_table "boards", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "author_name"
     t.string "title"
@@ -25,6 +25,7 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_20_012830) do
     t.string "password_digest"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
   end
 
 end
