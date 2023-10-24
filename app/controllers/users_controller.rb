d51aae47f8dcb018a7306cbb98042e094a1d18b5
tooya-a <ayaka.toya@divx.co.jp>
2023-10-24 16:24:43 +0900

    モデル追加

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -16,6 +16,7 @@ class UsersController < ApplicationController
     @user = User.find(params[:id])
   end
 
+
   private
 
   def user_params
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,16 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_10_20_015745) do
+ActiveRecord::Schema[7.0].define(version: 2023_10_24_072229) do
+  create_table "blogs", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
+    t.string "title"
+    t.text "content"
+    t.bigint "user_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["user_id"], name: "index_blogs_on_user_id"
+  end
+
   create_table "boards", charset: "utf8mb4", collation: "utf8mb4_0900_ai_ci", force: :cascade do |t|
     t.string "author_name"
     t.string "title"
@@ -28,4 +37,5 @@ ActiveRecord::Schema[7.0].define(version: 2023_10_20_015745) do
     t.index ["email"], name: "index_users_on_email", unique: true
   end
 
+  add_foreign_key "blogs", "users"
 end
