3cea8caa44c6bf301d17c8eec461542c6097fe60
tooya-a <ayaka.toya@divx.co.jp>
2023-10-26 14:56:37 +0900

    アソシエーションの実装

--- app/controllers/blogs_controller.rb
+++ app/controllers/blogs_controller.rb
@@ -9,15 +9,16 @@ class BlogsController < ApplicationController
 
   def create
     @blog = Blog.new(blog_params)
-      if @blog.save
-        redirect_to new_blogs_path
-      else
-        render :new
-      end
+    @blog.user_id = current_user.id
+    if @blog.save
+      redirect_to blogs_path, notice: "ブログを作成しました！"
+    else
+      render :new
+    end
   end
 
   def show
-    redirect_to new_blogs_path
+    @blog = Blog.find(params[:id])
   end
 
   private
