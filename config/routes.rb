f84b6b2884a43df265998de51f6bbde594f6dc2c
tooya-a <ayaka.toya@divx.co.jp>
2023-10-30 14:26:12 +0900

    /letter_openerにメールが届くように実装

--- config/routes.rb
+++ config/routes.rb
@@ -11,4 +11,5 @@ Rails.application.routes.draw do
   resources :users, only: [:new, :create, :show]
   resources :blogs
   resources :favorites, only: [:create, :destroy] 
+  mount LetterOpenerWeb::Engine, at: "/letter_opener" if Rails.env.development?
 end
